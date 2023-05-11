933f08bae6edf23a5133c1d8bdcf5afdfe3baabd
Lance Warshawsky <lance.warshawsky@gmail.com>
2023-05-11 11:57:43 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 const getCharacter = () => {
     fetch("https://bobsburgers-api.herokuapp.com/characters/1")
-      .then(response => response.json())
-      .then(data => {
+    .then(response => response.json())
+    .then(characters =>{
+        const character = characters[0];
         const characterName = document.createElement("h1"); 
-        characterName.textContent = data.name;
+        characterName.textContent = character.name;
         const characterElement = document.getElementById("burgers");
         characterElement.innerHTML = ""; 
-  
+
         characterElement.appendChild(characterName); 
-      })
-      .catch(err => {
+    })
+    .catch(err => {
         console.error(err.message);
-      });
-  };
-  
-  document.getElementById("getButton").addEventListener("click", getCharacter);
+    });
+};
+
+document.getElementById("getButton").addEventListener("click", getCharacter);
